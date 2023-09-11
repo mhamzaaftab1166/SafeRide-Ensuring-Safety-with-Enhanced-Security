70708bf54febc5c233b53a21e2b52902fcc5dfdb
Hamza Aftab <bhattihamza0099@gmail.com>
2023-09-11 15:55:40 +0500

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Dashboard from "./app/screens/owners/Dashboard";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App(props) {
+  return <Dashboard />;
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
+export default App;
---
+++ app/screens/owners/Dashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+function Dashboard(props) {
+  return (
+    <View style={styles.container}>
+      <Text>hi ridemate</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {},
+});
+export default Dashboard;
