81b256a7f6687968acf221dd70b970d921e4ba75
Hamza Aftab <bhattihamza0099@gmail.com>
2023-09-13 16:29:17 +0500

    implemented message screen and handled delete and refresh action

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,14 +1,110 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { FlatList, StyleSheet } from "react-native";
 
-const MessagesScreen = () => {
+import ListItem from "../components/ListItem";
+import ListItemSeprator from "../components/ListItemSeprator";
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
+
+const initialMessages = [
+  {
+    id: 1,
+    title: "hamza",
+    description: "this is my description",
+    image: require("../assets/hamza.jpeg"),
+  },
+  {
+    id: 2,
+    title: "hamza",
+    description: "this is my description",
+    image: require("../assets/hamza.jpeg"),
+  },
+  {
+    id: 3,
+    title: "hamza",
+    description: "this is my description",
+    image: require("../assets/hamza.jpeg"),
+  },
+  {
+    id: 4,
+    title: "hamza",
+    description: "this is my description",
+    image: require("../assets/hamza.jpeg"),
+  },
+  {
+    id: 5,
+    title: "hamza",
+    description: "this is my description",
+    image: require("../assets/hamza.jpeg"),
+  },
+  {
+    id: 6,
+    title: "hamza",
+    description: "this is my description",
+    image: require("../assets/hamza.jpeg"),
+  },
+  {
+    id: 7,
+    title: "hamza",
+    description: "this is my description",
+    image: require("../assets/hamza.jpeg"),
+  },
+  {
+    id: 8,
+    title: "hamza",
+    description: "this is my description",
+    image: require("../assets/hamza.jpeg"),
+  },
+  {
+    id: 9,
+    title: "hamza",
+    description: "this is my description",
+    image: require("../assets/hamza.jpeg"),
+  },
+  {
+    id: 10,
+    title: "hamza",
+    description: "this is my description",
+    image: require("../assets/hamza.jpeg"),
+  },
+];
+
+function MessagesScreen(props) {
+  const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleDelete = (message) => {
+    setMessages(messages.filter((m) => m.id !== message.id));
+  };
+
   return (
-    <View>
-      <Text>MessagesScreen</Text>
-    </View>
+    <FlatList
+      data={messages}
+      keyExtractor={(message) => message.id.toString()}
+      renderItem={({ item }) => (
+        <ListItem
+          title={item.title}
+          subTitle={item.description}
+          image={item.image}
+          onPress={() => console.log("messg tap", item)}
+          renderRightActions={() => (
+            <ListItemDeleteAction onPress={() => handleDelete(item)} />
+          )}
+        ></ListItem>
+      )}
+      refreshing={refreshing}
+      onRefresh={() =>
+        setMessages([
+          {
+            id: 2,
+            title: "hamza",
+            description: "this is my description",
+            image: require("../assets/hamza.jpeg"),
+          },
+        ])
+      }
+      ItemSeparatorComponent={ListItemSeprator}
+    />
   );
-};
-
-export default MessagesScreen;
-
+}
 const styles = StyleSheet.create({});
+export default MessagesScreen;
