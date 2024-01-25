45ef74cbcfd78ddf32d2cc4e492952bf64c7b748
Hamza Aftab <bhattihamza0099@gmail.com>
2024-01-25 17:28:35 +0500

    displayed the calculated distance

--- app/components/MapComponent.js
+++ app/components/MapComponent.js
@@ -1,27 +1,42 @@
+import React, { Component } from "react";
 import {
   Platform,
   PixelRatio,
-  Text,
   StyleSheet,
   View,
   StatusBar,
   Image,
 } from "react-native";
-import React, { Component } from "react";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import MapViewDirection from "react-native-maps-directions";
 import { mapStyle } from "../config/mapStyle";
 import colors from "../config/colors";
-import MapViewDirection from "react-native-maps-directions";
 import { GOOGLE_MAPS_APIKEY } from "@env";
-
+import { getDistance } from "geolib";
+import AppText from "./AppText";
 export default class MapComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      distance: null,
+    };
     this._map = React.createRef();
-    this.intervalId = null; // Initialize intervalId
+    this.intervalId = null;
   }
-
+  DistanceMarker = ({ distance }) => {
+    return (
+      <View style={styles.container}>
+        <MaterialCommunityIcons
+          name="map-marker-distance"
+          color={colors.black}
+          size={24}
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>{`Distance: ${distance} km`}</Text>
+        </View>
+      </View>
+    );
+  };
   componentDidMount() {
     this.intervalId = setInterval(() => {
       if (this.props.userDestination.latitude !== null) {
@@ -34,7 +49,18 @@ export default class MapComponent extends Component {
         };
         const edgePadding =
           Platform.OS === "android" ? androidEdgePadding : iosEdgePadding;
+        const distance = getDistance(
+          {
+            latitude: this.props.userOrigin.latitude,
+            longitude: this.props.userOrigin.longitude,
+          },
+          {
+            latitude: this.props.userDestination.latitude,
+            longitude: this.props.userDestination.longitude,
+          }
+        );
 
+        this.setState({ distance });
         this._map.current?.fitToCoordinates(
           [this.props.userOrigin, this.props.userDestination],
           {
@@ -43,11 +69,10 @@ export default class MapComponent extends Component {
           }
         );
       }
-    }, 500);
+    }, 1000);
   }
 
   componentWillUnmount() {
-    // Clear the interval when the component is unmounted
     clearInterval(this.intervalId);
   }
 
@@ -94,10 +119,61 @@ export default class MapComponent extends Component {
               strokeColor={colors.black}
             />
           )}
+          {this.state.distance !== null && (
+            <Marker
+              coordinate={{
+                latitude:
+                  (this.props.userOrigin.latitude +
+                    this.props.userDestination.latitude) /
+                  2,
+                longitude:
+                  (this.props.userOrigin.longitude +
+                    this.props.userDestination.longitude) /
+                  2,
+              }}
+              anchor={{ x: 0.5, y: 0.5 }}
+              title={`Distance: ${this.state.distance} meters`}
+            >
+              <View
+                style={{
+                  width: "100%",
+                  height: 40,
+                  backgroundColor: colors.white,
+                  borderRadius: 50,
+                }}
+              >
+                <AppText>{`Distance: ${
+                  this.state.distance / 1000
+                } km`}</AppText>
+              </View>
+            </Marker>
+          )}
         </MapView>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    top: 10,
+    left: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: colors.white,
+    padding: 5,
+    borderRadius: 5,
+    elevation: 3, // Android shadow
+    shadowColor: colors.black, // iOS shadow
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  textContainer: {
+    marginLeft: 5,
+  },
+  text: {
+    color: colors.black,
+  },
+});
